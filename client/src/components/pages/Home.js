d640de757c72ad614e3ca03b38ed9beeb294f790
Akash <akash.grep@gmail.com>
2020-07-06 15:01:40 +0530

    Design the Home page using Material ui and css . Add svg in bacckground Add buttons and texts titles.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,31 +1,74 @@
 import React from "react";
-
+import '../../App.css';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-
+import Container from '@material-ui/core/Container';
 import Navbar from "../layout/Navbar";
 import ScrollTopButton from "../layout/ScrollTopButton";
+
+
+const StyledButton = withStyles({
+  root: {
+    background: '#FF8E53',
+    borderRadius: 6,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    fontSize:'2vh',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})(Button);
 
 const Home = () => {
 
   
   return (
-    <div className="container">
+    <div>
+     
       <Navbar />
       <ScrollTopButton />
+      <div className="back-image"  >
+      <Container maxWidth="lg">
+      <Container>
+      <h1 className="title">Graphic Elements for Everyone</h1>
+      <p className="para">A set of small, responsive CSS modules that</p>
+      <p  className="para"> you can use in every web project,
+         portfolio or both.</p>
+         <p className="para" style={{paddingBottom:"5vh"}}> Download the CSS modules for free, or contribute!</p>
+     <StyledButton>Get Started</StyledButton>
+     &nbsp;
+     <Button variant="outlined" color="cyan" style={btnStyle}> Learn More</Button> 
+     
+        </Container>
+     
       
-      <h1>Home Page</h1>
-      <Button variant="outlined" color="inherit" style={btnStyle}> Get Started</Button>
-    </div>
+      </Container>
+     
+      </div>
+     
+               </div>
   );
 };
 const btnStyle = {
-  background: '#000',
-  color: '#fff',
-  border: 'none',
+  
+  
+  borderRadius: 6,
+  border: 1,
   padding:'5px 9px',
-horizontalAlign: 'center'
+  marginLeft:'3vh',
+  height: 48,
+  padding: '0 30px',
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+
  
 }
+// const customHome = {
+//   width:"inherit"
+// }
 
 
 export default Home;
