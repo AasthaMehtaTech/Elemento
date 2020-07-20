75bf1bc344f94485eeb3e10a1e369bc600358083
sarthakkundra <sarthakkundra21@gmail.com>
2020-07-21 00:55:15 +0530

    added view code modal

--- client/src/components/element/ElementItem.js
+++ client/src/components/element/ElementItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,29 +8,32 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
-
-
 import CodeIcon from "@material-ui/icons/Code";
 import GetAppRoundedIcon from "@material-ui/icons/GetAppRounded";
+
+import ElementModal from './ElementModal';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 300,
   },
-  
 });
+
 
 const ElementItem = ({ name, img }) => {
   const classes = useStyles();
 
+  const [isOpen, setIsOpen] = useState(false);
 
+  const handleOpen = () => {
+    setIsOpen(true);
+  }
   
-
-  
-
+  const handleClose = () => {
+    setIsOpen(false);
+  }
 
   return (
-    
     <div style={elementStyle}>
       <Card className={classes.root} style={cardStyle}>
         <CardActionArea>
@@ -62,14 +65,14 @@ const ElementItem = ({ name, img }) => {
             size='small'
             className={classes.button}
             startIcon={<CodeIcon />}
-            
+            onClick={handleOpen}
           >
             View Code
           </Button>
         </CardActions>
       </Card>
+      <ElementModal isOpen={isOpen} close={handleClose} />
     </div>
-  
   );
 };
 
