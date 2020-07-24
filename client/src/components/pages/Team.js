5a93e6e1705f029dc219e2058f0803d859748c50
Akash <akash.grep@gmail.com>
2020-07-24 12:44:21 +0530

    Made change in Team.js for adding images and content

--- client/src/components/pages/Team.js
+++ client/src/components/pages/Team.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import '../../App.css';
-
 import Footer from '../layout/Footer';
-
 import Navbar from '../layout/Navbar';
 import ScrollTopButton from '../layout/ScrollTopButton';
 import { Container } from '@material-ui/core';
-
+import backimage  from '../../img/Blueback.svg'
 const Team = () => {
     return (
+        
         <div >
             <Navbar />
             <ScrollTopButton />
-           <div className="teamBackground">
-         <Container >
-
-         </Container>
-           </div>
+           
+         <Container className="contentTeam" >
+     
+      <h1 className="titleTeam">About Us</h1>
+      <p className="paraTeam"> A set of small, responsive CSS modules that you can use in every web project,
+         portfolio or both.
+         A set of small, responsive CSS modules that you can use in every web project,
+         portfolio or both.
+         A set of small, responsive CSS modules that you can use in every web project,
+         portfolio or both.
+         A set of small, responsive CSS modules that you can use in every web project,
+         portfolio or both.
+         </p>
+      
+         
+        </Container> 
+        <img src={backimage} className="backg" >
+            </img>
+          
 
            <Footer />
         </div>
+        
     )
 }
 
