14b5cc4d952f62ff70017d5c87552844100c3073
sarthakkundra <sarthakkundra21@gmail.com>
2020-07-29 11:20:54 +0530

    Refactor #1

--- client/src/components/element/ElementView.js
+++ client/src/components/element/ElementView.js
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const ElementView = ({html, css, js}) => {
-var code = completeScript(html, css, js);
-    return (
-        <div>
-           <iframe style={{width: '100%', height: '200px', marginTop: '50px', border: '1px solid' }} srcDoc = {`${code}`}  />
-        </div>
-    )
-}
+import completeScript from "../../utils/completeScript";
 
-const completeScript = (html, css , js) => {
+const ElementView = ({ html, css, js }) => {
+  var code = completeScript(html, css, js);
+  return (
+    <div>
+      <iframe
+        style={{
+          width: "100%",
+          height: "200px",
+          marginTop: "50px",
+          border: "1px solid",
+        }}
+        srcDoc={`${code}`}
+      />
+    </div>
+  );
+};
 
-    var insert = html.indexOf('<head>');
-        insert += 7
-    var str1 = html.slice(0,insert);
-    var str2 = html.slice(insert);
-
-    var cssJs = `<style>\n ${css} \n</style>\n<script>\n${js}\n</script>\n`
-
-    return str1+cssJs+str2;
-}
-export default ElementView
+export default ElementView;
