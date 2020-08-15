21a4c4557f052bbc80bba7ec7ab0fa370a33f986
Akash <akash.grep@gmail.com>
2020-08-15 09:49:40 +0530

    Add different arrays for rendering images in example.js

--- client/src/components/layout/example.js
+++ client/src/components/layout/example.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import '../../App.css'
 import Paper from '@material-ui/core/Paper';
 import { Container } from '@material-ui/core';
-
+import photo from '../../img/AK.jpg'
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -26,6 +26,36 @@ const useStyles = makeStyles((theme) => ({
   // const handleChange = (event) => {
   //   setSpacing(Number(event.target.value));
   // };
+  const arr1 = [{
+
+    id:photo
+  },{
+
+    id:"https://mdbootstrap.com/wp-content/uploads/2015/08/navbar-fb.jpg"
+  },{
+    id:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGtcDYNlfc8JHSe3b5qGFBM0bvmuu7WNkt1w&usqp=CAU"
+  }
+  ]
+  const arr2 = [{
+
+    id:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGtcDYNlfc8JHSe3b5qGFBM0bvmuu7WNkt1w&usqp=CAU"
+  },{
+    
+    id:"https://mdbootstrap.com/wp-content/uploads/2015/08/navbar-fb.jpg"
+  },{
+    id:photo
+  }
+  ]
+  const arr3 = [{
+
+    id:"https://mdbootstrap.com/wp-content/uploads/2015/08/navbar-fb.jpg"
+  },{
+    
+    id:photo
+  },{
+    id:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSGtcDYNlfc8JHSe3b5qGFBM0bvmuu7WNkt1w&usqp=CAU"
+  }
+  ]
 
   return (
     <div className="sec-backImage">
@@ -36,18 +66,23 @@ const useStyles = makeStyles((theme) => ({
       <Grid container className={classes.root} spacing={8}>
         <Grid item xs={12}>
         <Grid container justify="center" spacing={spacing}>
-          {[0, 1, 2].map((value) => (
+          {arr1.map((value) => (
             <Grid key={value} item>
-              <Paper className={classes.paper}></Paper>
+              <Paper className={classes.paper}>
+
+              <img src={value.id} style={{width: '100%', height: '100%'}}/>
+              </Paper>
             </Grid>
           ))}
         </Grid>
       </Grid>
       <Grid item xs={12}>
         <Grid container justify="center" spacing={spacing}>
-          {[0, 1, 2].map((value) => (
+          {arr2.map((value) => (
             <Grid key={value} item>
-              <Paper className={classes.paper}></Paper>
+              <Paper className={classes.paper}>
+              <img src={value.id} style={{width: '100%', height: '100%'}}/>
+              </Paper>
             </Grid>
           ))}
         </Grid>
@@ -55,9 +90,11 @@ const useStyles = makeStyles((theme) => ({
 
       <Grid item xs={12}>
         <Grid container justify="center" spacing={spacing}>
-          {[0, 1, 2].map((value) => (
+          {arr3.map((value) => (
             <Grid key={value} item>
-              <Paper className={classes.paper}></Paper>
+              <Paper className={classes.paper}>
+              <img src={value.id} style={{width: '100%', height: '100%'}}/>
+              </Paper>
             </Grid>
           ))}
         </Grid>
