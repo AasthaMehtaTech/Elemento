b34c3bdb4fc96e1d48c304e9ebf14758b2d6d54c
sarthakkundra <sarthakkundra21@gmail.com>
2020-08-15 21:55:27 +0530

    team modal FIX

---
+++ client/src/components/pages/Contribute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navbar from '../layout/Navbar'
+
+const Contribute = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Contribute form</h1>
+        </div>
+    )
+}
+
+export default Contribute
