36520efa8882523e29a9e5dce66912057ae5aeb9
Akash <akash.grep@gmail.com>
2020-08-25 20:49:03 +0530

    Add a directory Data in src directory It is for storing Team Details

---
+++ client/src/Data/TeamDetails.js
@@ -0,0 +1,58 @@
+  
+  var fblink = "https://www.linkedin.com/in/grep-akash/";
+  var instalink = "www.google.com";
+  var githublink = "www.google.com";
+  var linkedinlink = "www.google.com";
+
+  var profileKey = [{
+    PersonName:"Akash Krishna",
+    PersonRole:"Software developer",
+    PersonBio:"Hi there!",
+    PersonImg:"https://raw.githubusercontent.com/akrishna0/Customizable_Portfolio/master/client/src/img/AK.jpg",
+
+    fblink:"https://www.facebook.com/akash.krishna.125",
+    instalink:"https://www.instagram.com/akrishna_ak/",
+    githublink:"https://github.com/akrishna0",
+    linkedinlink:"https://www.linkedin.com/in/grep-akash/",
+    bio:"This is Akash Krishna  an Undergrad Student , Full Stack Developer and Cyber Security Enthusiast"
+  },
+  {
+    PersonName:"Sarthak Kundra",
+    PersonRole:"Software Developer",
+    PersonBio:"Hi there!",
+    PersonImg:"https://avatars2.githubusercontent.com/u/52133994?s=460&u=c025480f6def3f6600d358ae8d9eea23a0b56bcb&v=4",
+
+    fblink:{fblink},
+    instalink:{instalink},
+    githublink:"https://github.com/sarthakkundra",
+    linkedinlink:"https://www.linkedin.com/in/sarthak-kundra-2b6247189/",
+    bio:"Sarthak is a MERN stack developer making web apps to make everyday lives easier. He's a keen learner and has an inquisitive personality which makes him want to explore new avenues whenever possible. He's currently exploring the world of machine learning."
+  },
+  {
+    PersonName:"Aastha Mehta",
+    PersonRole:"Software developer",
+    PersonBio:"Hi there!",
+    PersonImg:"https://avatars2.githubusercontent.com/u/49694058?s=460&u=a9875532cc418eb06d3b034b1d5aeea27d431bb0&v=4",
+
+    fblink:{fblink},
+    instalink:{instalink},
+    githublink:"https://github.com/AasthaGithub",
+    linkedinlink:"https://www.linkedin.com/in/aastha-mehta-176030179",
+    bio:"Aastha is a curious learner, passionate about Technology that touches lives. She's had experience in Machine Learning, web & app domains. Her toolbox includes Python, Java, SQL, Web Tech & is ever expanding. In the pursuit of expanding knowledge base & exploring fun things, she loves to resort to books & shows!"
+  },
+  {
+    PersonName:"Vidit Jindal",
+    PersonRole:"Software developer",
+    PersonBio:"Hi there!",
+    PersonImg:"https://instagram.fpat1-1.fna.fbcdn.net/v/t51.2885-19/s320x320/54800493_1169846746518463_4712422096943185920_n.jpg?_nc_ht=instagram.fpat1-1.fna.fbcdn.net&_nc_ohc=Os6m5ZCZdMAAX-NxUBm&oh=06326373568cebbae2ca0cf63babd730&oe=5F6EE3CD",
+
+    fblink:{fblink},
+    instalink:"https://www.instagram.com/viditjindal_20/",
+    githublink:"https://github.com/Vidit-jindal",
+    linkedinlink:"https://www.linkedin.com/in/vidit-jindal-53270a182/",
+    bio:"This is Vidit Jindal from Jaypee University of Information Technology, Solan . He is a Software and Web Developer and is exploring the concepts of Machine Learning and Data Science "
+  },
+   
+];
+
+export {profileKey};
