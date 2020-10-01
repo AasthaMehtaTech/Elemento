e97fc7d7de66978fcc857b5f32be4020a0863387
sohamsshah <sohamshah456@gmail.com>
2020-10-02 00:45:40 +0530

    change the Navbar hover effect on Navlinks

--- client/src/components/pages/Team.js
+++ client/src/components/pages/Team.js
@@ -33,8 +33,9 @@ const Team = () => {
     <Navbar />
     <ScrollTopButton /> 
     <Container className="contentTeam">
-      <h1 className="titleTeam">About Us</h1>
-      <p className="paraTeam">Customizable Portfolio is a website that aims to help people make a fully functional static website in a short amount of time without relying on web developers and that too with very little prior programming experience.
+      <h1 className="titleTeam"> <b> About Us</b></h1>
+      <p className="paraTeam">
+      Customizable Portfolio is a website that aims to help people make a fully functional static website in a short amount of time without relying on web developers and that too with very little prior programming experience.
 
 This project is part of PSoC (Program Summer of Code) organized by UIET Chandigarh. It is a 2 month long open source competition to help people get started with open source with the help of experienced mentors.
       </p>      
